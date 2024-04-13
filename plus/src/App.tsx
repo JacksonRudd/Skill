89e4c2677d4e095493e9490a6910f36247cc60ed
Jackson Rudd <jarudd@microsoft.com>
2024-04-13 08:55:31 -0700

    simple game working

--- plus/src/App.tsx
+++ plus/src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import Problem from './components/problem'
 
 function App() {
-  return <Problem a={4} b={5} />
+  const [a, setA] = useState(4)
+  const [b, setB] = useState(5)
+
+  function correct() {
+    setA(Math.floor(Math.random() * 100) + 1)
+    setB(Math.floor(Math.random() * 100) + 1)
+  }
+
+  return <Problem a={a} b={b} onAnswerCorrect={correct} />
 }
 
 export default App
--- plus/src/components/problem.tsx
+++ plus/src/components/problem.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 
-function Problem({ a, b }: { a: number; b: number }) {
+function Problem({
+  a,
+  b,
+  onAnswerCorrect,
+}: {
+  a: number
+  b: number
+  onAnswerCorrect: () => void
+}) {
   const [inputValue, setInputValue] = useState('')
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
     if (e.target.value === String(a + b)) {
-      alert('correct!')
+      onAnswerCorrect()
+      setInputValue('')
     }
   }
 
