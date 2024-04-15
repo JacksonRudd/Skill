3bdc50b0797dffefd575ef6de8d7877108083c6e
Jackson Rudd <jarudd@microsoft.com>
2024-04-15 06:17:21 -0700

    added nav bar

--- plus/src/App.tsx
+++ plus/src/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 import Game from './components/game'
-
+import History from './components/history'
+import About from './components/about'
 function App() {
+  const [page, setPage] = useState<'history' | 'about' | 'game'>('history')
+
+  const handlePageChange = (newPage: 'history' | 'about' | 'game') => {
+    setPage(newPage)
+  }
+
   return (
     <>
-      <Game>{}</Game>
+      <nav>
+        <ul>
+          <li>
+            <a href="#" onClick={() => handlePageChange('history')}>
+              History
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={() => handlePageChange('game')}>
+              Play
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={() => handlePageChange('about')}>
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {page === 'history' && <History />}
+      {page === 'game' && <Game />}
+      {page === 'about' && <About />}
     </>
   )
 }
