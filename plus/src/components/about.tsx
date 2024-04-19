ec167c99b511d573a6c8ed4627bbcb756848e417
Jackson Rudd <jarudd@microsoft.com>
2024-04-19 09:04:44 -0700

    done

--- plus/src/components/about.tsx
+++ plus/src/components/about.tsx
@@ -3,37 +3,37 @@ function About() {
     <div style={{ textAlign: 'center', maxWidth: '600px', margin: '0 auto' }}>
       <h1>Welcome to Plus!</h1>
       <p>
-        I tried to create a game that would make learning addition feel relaxing
-        and pleasent.
+        Zenofcal is designed to transform learning addition into a soothing and
+        enjoyable experience.
       </p>
 
       <h2>How to Input Answers</h2>
       <p>
-        You can input your answers using either a standard numpad or our special
-        key mappings for added convenience:
+        Answers can be input using either a standard numpad or via special key
+        mappings for enhanced ease of use:
       </p>
       <ul>
-        <li>'m' maps to 1</li>
-        <li>',' (comma) maps to 2</li>
-        <li>'.' (period) maps to 3</li>
-        <li>'j' maps to 4</li>
-        <li>'k' maps to 5</li>
-        <li>'l' maps to 6</li>
-        <li>'u' maps to 7</li>
-        <li>'i' maps to 8</li>
-        <li>'o' maps to 9</li>
-        <li>'h' functions as a backspace for correcting entries</li>
+        <li>'m' corresponds to 1</li>
+        <li>',' (comma) corresponds to 2</li>
+        <li>'.' (period) corresponds to 3</li>
+        <li>'j' corresponds to 4</li>
+        <li>'k' corresponds to 5</li>
+        <li>'l' corresponds to 6</li>
+        <li>'u' corresponds to 7</li>
+        <li>'i' corresponds to 8</li>
+        <li>'o' corresponds to 9</li>
+        <li>'h' serves as a backspace for correcting entries</li>
         <li>':' (colon) represents the number '0'</li>
       </ul>
 
       <h2>Scoring System</h2>
       <p>
-        Solving a problem in less than two seconds will increase your score,
-        while taking longer will decrease it. The nubers you are adding together
-        are always less than your score, so as your score increases, the
-        difficulty will as well. This means in the beggining of them game you
-        will solve very simple problems. You'll never loose more than twopoints
-        in a round, so don't stress about leaving your computer.
+        Swift responses within two seconds enhance your score, while more
+        time-consuming efforts will gently reduce it. The numbers presented for
+        addition are always less than your current score, so the difficulty
+        naturally increases as you progress. Fear not, as you will never lose
+        more than two points in a round, allowing for a stress-free experience
+        even if you need to step away from the computer.
       </p>
     </div>
   )
